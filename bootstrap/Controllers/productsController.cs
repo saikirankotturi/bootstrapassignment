10d1461d9fa826cee25ba2dd1850abbc2426caaf
sakottur <sakottur@DIN76000832>
2017-12-26 14:21:49 +0530

    Add project files.

---
+++ bootstrap/Controllers/productsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class productsController : Controller
+    {
+        // GET: products
+
+
+
+        public ActionResult products()
+        {
+
+
+            return View();
+        }
+
+
+        public ActionResult laptop()
+        {
+            return View();
+        }
+
+        public ActionResult Tv()
+        {
+            return View();
+        }
+
+        public ActionResult microwave()
+        {
+            return View();
+        }
+
+        public ActionResult Mobile()
+        {
+            return View();
+        }
+    }
+}
---
+++ bootstrap/Controllers/serviceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class serviceController : Controller
+    {
+        // GET: service
+
+        public ActionResult services()
+        {
+            return View();
+        }
+        public ActionResult laptopservice()
+        {
+            return View();
+        }
+        public ActionResult Tvservice()
+        {
+            return View();
+        }
+
+        public ActionResult Mobileservice()
+        {
+            return View();
+        }
+
+        public ActionResult microwaveservice()
+        {
+            return View();
+        }
+    }
+}
